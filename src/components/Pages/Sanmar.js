9a2cfd6acc65e2ee6c29162f928aa0bb7722171a
Tre Olson <treolson@live.com>
2022-02-11 15:57:29 -0600

    more sanmar stuff

--- src/components/Pages/Sanmar.js
+++ src/components/Pages/Sanmar.js
@@ -18,39 +18,98 @@ function Sanmar () {
 
   const SanmarItems = useSelector(store => store.item.clothinglist);
   const BcItems = useSelector(store => store.item.bcClothinglist);
-  const [host, setHost] = useState('');
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
+  const [host, setHost] = useState('ftp.sanmar.com');
+  const [user, setUser] = useState('175733');
+  const [password, setPassword] = useState('Sanmar33');
   const dispatch = useDispatch();
+//   const reader = readData();
   const Client = require('ftp');
-  const fs = require('fs');
-  const c = new Client();
-
-  c.on('ready', function () {
-      c.get('foo.txt', function (err, stream) {
-          if (err) throw err;
-          stream.once('close', function () {
-              c.end();
-          });
-          stream.pipe(fs.createWriteStream('foo.local-copy.txt'));
-      });
-  });
-
-
-  const connectFtp = () => {  
-  console.log('Logging into FTP Client..');
   
-  c.connect({
-      host: host,
-      port: 21,
-      user: user,
-      password: password,
-  });
 
-  setHost('');
-  setUser('');
-  setPassword('');
-}
+
+async function connectFtp() {
+    console.log('Logging into FTP Client..');
+
+    let c = new Client();
+    const fs = require("fs");
+
+    let ftpConfig = {
+      host: `${host}`,
+      port: 21,
+      user: `${user}`,
+      password: `${password}`,
+    }
+    //console.log(`${host}, ${user}, ${password}`);
+
+    c.connect(ftpConfig);
+
+    c.on('ready', function () {
+      c.get(`/000175733Status/02-09-22status.txt`, function (err, stream) {
+        if (err) throw err;
+        stream.once('close', function () {
+          c.end();
+        });
+        stream.pipe(fs.createWriteStream('local-copy.txt'));
+      });
+    });
+
+    // const client = new ftp.Client()
+    // client.ftp.verbose = true
+    // try {
+    //   await client.access({
+    //     host: host,
+    //     user: user,
+    //     port: 21,
+    //     password: password,
+    //     secure: false
+    //   })
+
+    //   //await client.downloadTo(stream, `/000175733Status/02-09-22status.txt`);
+
+    // } catch (err) {
+    //   console.log(err);
+    // }
+    // client.close();
+
+      setHost('');
+      setUser('');
+      setPassword('');
+  }
+
+// function readData(writer, data, encoding, callback) {
+
+//     console.log(writer);
+//     //write();
+
+//     function write() {
+//       let ok = true;
+//         if (true) {
+//           // Last time!
+//           writer.write(data, encoding, callback);
+//         } else {
+//           // See if we should continue, or wait.
+//           // Don't pass the callback, because we're not done yet.
+//           ok = writer.write(data, encoding);
+//         }
+//       if (true) {
+//         // Had to stop early!
+//         // Write some more once it drains.
+//         writer.once('drain', write);
+//       }
+//     }
+//   }
+
+// const stream = (data) => {
+
+//   const writer = fs.createWriteStream(`${data}`);
+
+//   reader.pipe(writer);
+
+//   writer.on('pipe', (src) => {
+//     console.log(`Something is piping into the writer. -- ${src}`);
+//   });
+
+// }
 
   const updatePrices = () => {
     if (BcItems[0]) {
@@ -101,7 +160,7 @@ function Sanmar () {
       <section className="sanmar-form">
         <h1>Sync Clothing Prices on BigCommerce</h1>
       </section>
-      <seciton className="sanmar-form">
+      <section className="sanmar-form">
       <div className="container">
         <div className="row">
         <div className="clothing-importer">
@@ -161,7 +220,7 @@ function Sanmar () {
         </div>
       </div>
       </div>
-      </seciton>
+      </section>
       <section className="total-form">
       <Button onClick={(e) => {updatePrices()}} className='sales-input'><QueueIcon/> Update Prices</Button>
       </section>
