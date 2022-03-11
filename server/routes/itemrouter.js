1c9481bd273d38fc3055aa161e5195038fdf0910
Tre Olson <treolson@live.com>
2022-03-11 14:38:59 -0600

    test

--- server/routes/itemrouter.js
+++ server/routes/itemrouter.js
@@ -39,7 +39,7 @@ async function updatePrices(bc, sanmar) {
   try {
       if (bc[0]) {
         for (const item of bc) {
-          if (item.sku > 5927) {
+          if (item.sku > 6320) {
           console.log(`Updating Product with ID: ${item.sku}`);
           await eachPrice(item, sanmar);
           await timeoutPromise(500);
