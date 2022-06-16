cb6f79bd1098de9e353c6f911ac950fbcf512220
Tre Olson <treolson@live.com>
2022-06-16 13:11:50 -0500

    change err log

--- server/routes/itemrouter.js
+++ server/routes/itemrouter.js
@@ -1603,7 +1603,7 @@ router.post("/bcRegister", cors(), async function (req, res) {
       inksoftConfig
     );
   } catch (err) {
-    console.log('Error on Register User: ', err, err.responseText);
+    console.log('Error on Register User: ', err.data.messages);
   }
 
   console.log('Registered: ', reg);
@@ -1628,7 +1628,7 @@ router.post("/bcRegister", cors(), async function (req, res) {
       inksoftConfig
     );
   } catch (err) {
-    console.log('Error on Login User: ', err, err.responseText);
+    console.log('Error on Login User: ', err.data.messages);
   }
 
   console.log('Signed In: ', signin);
@@ -1646,7 +1646,7 @@ router.post("/bcRegister", cors(), async function (req, res) {
       inksoftConfig
     );
   } catch (err) {
-    console.log('Error on Verify Session: ', err, err.responseText);
+    console.log('Error on Verify Session: ', err.data.messages);
   }
 
   console.log('Verified: ', verify);
